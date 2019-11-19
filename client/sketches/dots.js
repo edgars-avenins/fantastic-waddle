ad019dd3250da99a1fa497674372042368659d54
Student <student@eda-bc-01.localdomain>
2019-11-20 08:41:13 +1300

    planets spinnin

--- client/sketches/dots.js
+++ client/sketches/dots.js
@@ -4,6 +4,8 @@ export default function sketch(p){
     let height = window.innerHeight
     let angle = 0
     let button
+    let slider 
+    let multi
     let pause = false
 
     p.setup = () => {
@@ -12,6 +14,10 @@ export default function sketch(p){
       p.rectMode(p.CENTER)
       p.frameRate(30)
       pauseButton()
+
+      slider = p.createSlider(1,1.05,1,0.0001)
+      slider.position(100,20)
+      slider.style('width', '80px')
       
     }
     
@@ -22,27 +28,63 @@ export default function sketch(p){
       p.background(0)
       p.draw = () => {
         p.clear()
+        multi = slider.value()
         //understand reacts role in this
-        p.background(204);
+
+        p.push()
+        let mercury = new Circle(80,80,8, width/2,height/2)
+        mercury.display(angle * 4)
+        p.pop()
+
+        p.push()
+        let venus = new Circle(105,105,20, width/2,height/2)
+        venus.display(angle * 1.5)
+        p.pop()
+
+        p.push()
+        let earth = new Circle(130,130,20, width/2,height/2)
+        earth.display(angle)
+        p.pop()
+
+        p.push()
+        let mars = new Circle(150,150,11, width/2,height/2)
+        mars.display(angle / 2)
+        p.pop()
+
+        p.push()
+        let jupiter = new Circle(230,230,120, width/2,height/2)
+        jupiter.display(angle / 12)
+        p.pop()
+
+        p.push()
+        let saturn = new Circle(330,330,110, width/2,height/2)
+        saturn.display(angle / 30)
+
+        let ring = new Circle(0,0,120, 330,330)
+        ring.display(angle / 30)
+
+        p.pop()
+
+        p.push()
+        let uranus = new Circle(400,400,40, width/2,height/2)
+        uranus.display(angle / 84)
+        p.pop()
+
+        p.push()
+        let neptune = new Circle(460,460,40, width/2,height/2)
+        neptune.display(angle / 165)
+        p.pop()
+
+        p.push()
+        let pluto = new Circle(500,500,6, width/2,height/2)
+        pluto.display(angle / 248)
+        p.pop()
         
         p.translate(width / 2, height / 2)
-        p.circle(0,0,100)
-
-        p.rotate(angle)
-        p.circle(200, 0, 50)
-        
-        p.translate(200, 0)
-        
-        p.rotate(angle * 2)
-        p.circle(50, 0, 25)
-
-        p.translate(50, 0)
-        
-        p.rotate(angle * 5)
-        p.circle(20, 0, 5)
+        p.circle(0,0,180)
 
 
-        angle +=1
+        angle = multi * angle + 0.1
       }
     }
 
@@ -58,10 +100,20 @@ export default function sketch(p){
         }
       })
     }
-}
-//get a constructor going and create planets in this way
-class Planets{
-  constructor(){
 
-  }
+    function Circle(x,y,r,xTrans,yTrans){
+      this.x = x,
+      this.y = y,
+      this.r = r,
+      this.xTrans = xTrans,
+      this.yTrans = yTrans,
+
+      this.display = function(angle){
+        p.stroke(0)
+        p.noFill()
+        p.translate(this.xTrans, this.yTrans)
+        p.rotate(angle)
+        p.circle(this.x, this.y, this.r)
+      }
+    }
 }
